bcb96fba2a2f9dee1dfc5d6bb92f5fe3bb8332a2
Joel Jakobsson <joel@compiler.org>
2018-11-12 09:57:25 +0100

    Clear graph if input field is empty and change bgcolor to red on parse error

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ function(event){
     document.getElementById("regexp-input").addEventListener("keyup", function(event) {
         let regexinput = document.getElementById("regexp-input").value;
         if (regexinput.length == 0) {
+            document.getElementById("graph").innerHTML = "";
             return;
         }
-        d3.select("#graph").graphviz().renderDot(ReasonRegex.regexp2dot(regexinput));
+        try {
+            d3.select("#graph").graphviz().renderDot(ReasonRegex.regexp2dot(regexinput));
+            document.getElementById("regexp-input").style.backgroundColor = "";
+        } catch (e) {
+            document.getElementById("regexp-input").style.backgroundColor = "#ffaaaa";
+        }
     });
 });
 
